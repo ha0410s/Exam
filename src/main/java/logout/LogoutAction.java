6d023dfc2aed5311ed470929ff3625d9b03be16c
r_taguchi <r_taguchi@LAPTOP-83JL8F8D>
2024-04-15 10:01:37 +0900

    ログアウト

---
+++ src/main/java/logout/LogoutAction.java
@@ -0,0 +1,23 @@
+package logout;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class LogoutAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session=request.getSession();
+
+		if (session.getAttribute("teacher")!=null) {
+			session.removeAttribute("teacher");
+			return "logout.jsp";
+		}
+		
+		return "logout_error.jsp";
+	}
+}
+
